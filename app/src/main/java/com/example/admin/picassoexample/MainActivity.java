f2af8ca4a557e8905a8539f582780193297c3d77
Draldo <draldo_lopez@hotmail.com>
2016-07-29 15:35:47 -0400

    Picasso and gradle libraries

--- app/src/main/java/com/example/admin/picassoexample/MainActivity.java
+++ app/src/main/java/com/example/admin/picassoexample/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.admin.picassoexample;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity{
+
+    private ImageView imageView;
+    private ImageView imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void doMagic(View view) {
+
+        imageView = (ImageView) findViewById(R.id.a_main_imgvw);
+        imageView2 = (ImageView) findViewById(R.id.a_main_imgvw2);
+
+        Picasso
+                .with(this)
+                .load("http://www.gammerson.com/wp-content/uploads/2016/07/SrL1GaG.jpg")
+                .into(imageView);
+
+        GlideDrawableImageViewTarget gitImage = new GlideDrawableImageViewTarget(imageView2);
+        Glide
+                .with(this)
+                .load("http://vignette4.wikia.nocookie.net/narutofanon/images/6/62/Sharingan_eyes.gif")
+                .into(gitImage);
+    }
 }
+
